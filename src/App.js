1d5487924407e0b1f543a9b87dc61cfcf6234415
Asish Datta <87273349+AsishDatta@users.noreply.github.com>
2024-06-06 11:42:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AppBar, Toolbar, Typography, Container } from '@mui/material';
+import UsersList from './UsersList';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">
+                        API App
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Container>
+                <UsersList />
+            </Container>
+        </div>
+    );
+};
 
 export default App;
